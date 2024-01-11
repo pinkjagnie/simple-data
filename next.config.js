4c6ff1b66d8b0d0e1729a327a19dc51743902bb1
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-11 13:39:20 +0100

    Fixed issue with creating new user in db

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_APP_PB_URL: process.env.NEXT_APP_PB_URL,
+  },
   images: {
     domains: ["images.pexels.com"],
   },
