b83ebb7cff7716f1e9b9293b831790345b587cc4
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-11 13:44:45 +0100

    Added showing Message component to register proccess

--- src/app/components/forms/Register.js
+++ src/app/components/forms/Register.js
@@ -1,12 +1,20 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import { generateRandomId } from "@/utils/generateRandomUserID";
 
 import { useAuth } from "@/context/AuthContext";
 
 import RegisterForm from "./RegisterForm";
+import Message from "./Message";
 
 const Register = () => {
+  const [message, setMessage] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const router = useRouter();
+
   const { register } = useAuth();
 
   const onSubmit = async (values, actions) => {
@@ -26,16 +34,41 @@ const Register = () => {
         randomUserId
       );
 
-      console.log("done with success");
-
       actions.resetForm();
+
+      window.scrollTo(0, 0); // automatic scroll to top of the page
+
+      setMessage(
+        "User successfully created! You can now login to your account"
+      );
+
+      const timeout = setTimeout(() => {
+        router.push("/login");
+        clearTimeout(timeout);
+      }, 2000);
     } catch (error) {
-      console.log("upssssssssssss");
       console.log(error);
+
+      window.scrollTo(0, 0); // automatic scroll to top of the page
+
+      setErrorMsg("Something went wrong! " + error.data.message);
+
+      // clearing the error message
+      const timeout = setTimeout(() => {
+        setErrorMsg("");
+        clearTimeout(timeout);
+      }, 3000);
     }
   };
 
-  return <RegisterForm onSubmit={onSubmit} />;
+  return (
+    <>
+      {(message || errorMsg) && (
+        <Message message={message} errorMsg={errorMsg} />
+      )}
+      <RegisterForm onSubmit={onSubmit} />
+    </>
+  );
 };
 
 export default Register;
