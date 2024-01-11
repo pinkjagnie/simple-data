1e39586986b46277c2cf259fc5f4060cb1e05af7
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-11 17:48:31 +0100

    Added Sidebar for admin panel (force sidebar open)

---
+++ src/app/components/admin/Sidebar.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <div className="drawer drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content flex flex-col items-center justify-center">
+          {/* Sidebar content here */}
+          <li>
+            <Link href="/admin/currency-rate-table">Currency rate table</Link>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
