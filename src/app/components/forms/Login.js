4cb594a182b668f2541684631e89c2c2527222c9
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-11 14:20:56 +0100

    Added logic to login with displaying messages during login proccess

--- src/app/components/forms/Login.js
+++ src/app/components/forms/Login.js
@@ -1,17 +1,54 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { useAuth } from "@/context/AuthContext";
+
 import LoginForm from "./LoginForm";
 import Message from "./Message";
 
 const Login = () => {
+  const [errorMsg, setErrorMsg] = useState("");
+  const router = useRouter();
+
+  const { login } = useAuth();
+
   const onSubmit = async (values, actions) => {
     console.log(values);
 
     const enteredEmail = values.email;
     const enteredPass = values.password;
+
+    try {
+      await login(enteredEmail, enteredPass);
+
+      actions.resetForm();
+
+      window.scrollTo(0, 0); // automatic scroll to top of the page
+
+      router.push("/");
+    } catch (error) {
+      console.log(error.data.message);
+
+      window.scrollTo(0, 0); // automatic scroll to top of the page
+
+      setErrorMsg("Something went wrong. " + error.data.message);
+
+      // clearing the error message
+      const timeout = setTimeout(() => {
+        setErrorMsg("");
+        clearTimeout(timeout);
+      }, 3000);
+    }
   };
 
-  return <LoginForm onSubmit={onSubmit} />;
+  return (
+    <>
+      {errorMsg && <Message errorMsg={errorMsg} />}
+      <LoginForm onSubmit={onSubmit} />
+    </>
+  );
 };
 
 export default Login;
