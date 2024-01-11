dc34fb13b550b881698a4428cde36831d9f95902
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-11 14:12:14 +0100

    Added LoginForm

--- src/app/components/forms/Login.js
+++ src/app/components/forms/Login.js
@@ -1,7 +1,17 @@
+"use client";
+
 import LoginForm from "./LoginForm";
+import Message from "./Message";
 
 const Login = () => {
-  return <LoginForm />;
+  const onSubmit = async (values, actions) => {
+    console.log(values);
+
+    const enteredEmail = values.email;
+    const enteredPass = values.password;
+  };
+
+  return <LoginForm onSubmit={onSubmit} />;
 };
 
 export default Login;
--- src/app/components/forms/LoginForm.js
+++ src/app/components/forms/LoginForm.js
@@ -1,5 +1,44 @@
-const LoginForm = () => {
-  return <div>formularz to be</div>;
+"use client";
+
+import { Form, Formik } from "formik";
+
+import { loginSchema } from "@/schemas/index";
+
+import CustomInput from "./custom/CustomInput";
+import CustomPasswordInput from "./custom/CustomPasswordInput";
+
+const LoginForm = ({ onSubmit }) => {
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={loginSchema}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form className="form-control w-[90%] md:w-[60%] lg:w-[50%] p-6 mx-auto mt-2 mb-14 border-2 border-stone-200 rounded-md bg-stone-200">
+          <CustomInput label="Email" name="email" type="email" />
+          <CustomPasswordInput label="Password" name="password" />
+          {!isSubmitting ? (
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className="btn btn-outline bg-zinc-400 w-[80%] mx-auto block"
+            >
+              Login
+            </button>
+          ) : (
+            <button className="btn btn-active btn-accent text-slate-50 w-[80%] mx-auto block">
+              <span className="loading loading-spinner text-slate-50"></span>
+              Please wait
+            </button>
+          )}
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default LoginForm;
