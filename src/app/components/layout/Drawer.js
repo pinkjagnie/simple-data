493b4d7f7e37ff7401a9aba27504b4da671e44f9
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-11 16:43:42 +0100

    Set up Drawer visible only if user is logged in

---
+++ src/app/components/layout/Drawer.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Drawer = ({ openDrawer, toggleDrawer }) => {
+  return (
+    <div
+      className={
+        openDrawer
+          ? "fixed top-0 left-0 w-full md:w-[50%] lg:w-[30%] flex flex-col justify-center items-center h-screen bg-base-200 text-center ease-in duration-200 z-40"
+          : "fixed top-0 left-[-100%] ease-in"
+      }
+    >
+      <div className="w-[100%]">
+        <ul className="menu p-4 w-[100%] min-h-full bg-base-200 text-base-content font-medium px-6">
+          <li>
+            <Link href="/" onClick={() => toggleDrawer(openDrawer)}>
+              Some Item 1
+            </Link>
+          </li>
+          <li>
+            <Link href="/" onClick={() => toggleDrawer(openDrawer)}>
+              Some Item 2
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Drawer;
--- src/app/components/layout/Navbar.js
+++ src/app/components/layout/Navbar.js
@@ -8,9 +8,15 @@ import { useAuth } from "@/context/AuthContext";
 
 import { TbWorldSearch } from "react-icons/tb";
 
+import Drawer from "./Drawer";
+
 const Navbar = () => {
   const [dropDownContent, setDropDownContent] = useState(false);
   const { user, logout } = useAuth();
+
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggleDrawer = (prevState) => setOpenDrawer(!prevState);
 
   useEffect(() => {
     if (user) {
@@ -21,69 +27,100 @@ const Navbar = () => {
   }, [user]);
 
   return (
-    <div className="navbar bg-base-300">
-      <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-xl">
-          <TbWorldSearch size={30} />
-          World Data
-        </Link>
-      </div>
-      {/* HAMBURGER */}
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-square btn-ghost">
-            {" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
+    <>
+      <Drawer openDrawer={openDrawer} toggleDrawer={toggleDrawer} />
+      <div className="navbar fixed bg-base-300 z-50">
+        {/* DRAWER BUTTON */}
+        {dropDownContent ? (
+          <div className="flex-none">
+            <button
+              className="btn btn-square btn-ghost"
+              onClick={() => toggleDrawer(openDrawer)}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                ></path>
+              </svg>
+            </button>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
-          >
-            {dropDownContent ? (
-              <>
-                <li>
-                  <div className="avatar font-semibold">
-                    <p className="pr-2">Hello, {user.name}</p>
-                    <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                      <Image
-                        src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                        width={48}
-                        height={48}
-                        alt="user avatar"
-                      />
+        ) : null}
+        {/* LOGO */}
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl">
+            <TbWorldSearch size={30} />
+            World Data
+          </Link>
+        </div>
+        {/* HAMBURGER */}
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-square btn-ghost"
+            >
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+            >
+              {dropDownContent ? (
+                <>
+                  <li>
+                    <div className="avatar font-semibold">
+                      <p className="pr-2">Hello, {user.name}</p>
+                      <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <Image
+                          src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                          width={48}
+                          height={48}
+                          alt="user avatar"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </li>
-                <li>
-                  <button onClick={logout}>Logout</button>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link href="/login">Login</Link>
-                </li>
-                <li>
-                  <Link href="/register">Register</Link>
-                </li>
-              </>
-            )}
-          </ul>
+                  </li>
+                  <li>
+                    <button onClick={logout}>Logout</button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link href="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link href="/register">Register</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
