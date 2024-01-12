1525b5f64069795e62499c366a930deb90ea1341
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-12 10:25:23 +0100

    Fetching from API & next displaying gold statistics

---
+++ src/app/api/gold-statistics/route.js
@@ -0,0 +1,16 @@
+export async function GET(req, res) {
+  if (req.method !== "GET") {
+    return;
+  }
+
+  try {
+    const response = await fetch("http://api.nbp.pl/api/cenyzlota/last/2");
+    const prices = await response.json();
+    // console.log(currencies);
+
+    return Response.json(prices);
+  } catch (error) {
+    console.log(error);
+    return Response.error({ message: "Ups, something went wrong" });
+  }
+}
--- src/app/components/admin/gold-statistics/GoldStats.js
+++ src/app/components/admin/gold-statistics/GoldStats.js
@@ -1,27 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const GoldStats = () => {
+  const [goldPrices, setGoldPrices] = useState("");
+  const [priceDifference, setPriceDifference] = useState(0);
+  const [percentageDifference, setPercentageDifference] = useState(0);
+
+  const getGoldPrices = async () => {
+    const response = await fetch("/api/gold-statistics", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+    setGoldPrices(data);
+
+    setPriceDifference((data[1].cena - data[0].cena).toFixed(2));
+
+    setPercentageDifference(
+      (((data[1].cena - data[0].cena) / data[0].cena) * 100).toFixed(2)
+    );
+  };
+
+  useEffect(() => {
+    getGoldPrices();
+  }, []);
+
   return (
     <div>
       <p>Statistics for gold</p>
 
-      <div className="stats stats-vertical shadow">
-        <div className="stat">
-          <div className="stat-title">Gold price of yesterday</div>
-          <div className="stat-value">259,14 zł / g</div>
-          <div className="stat-desc">2024-01-11</div>
-        </div>
+      {goldPrices && (
+        <div className="stats stats-vertical shadow">
+          <div className="stat">
+            <div className="stat-title">Gold price of yesterday</div>
+            <div className="stat-value">{goldPrices[0].cena} zł / g</div>
+            <div className="stat-desc">{goldPrices[0].data}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-title">Gold price of today</div>
-          <div className="stat-value">258,40 zł / g</div>
-          <div className="stat-desc">↗︎ 2024-01-12</div>
-        </div>
+          <div className="stat">
+            <div className="stat-title">Gold price of today</div>
+            <div className="stat-value">{goldPrices[1].cena} zł / g</div>
+            <div className="stat-desc">{goldPrices[1].data}</div>
+          </div>
 
-        <div className="stat">
-          <div className="stat-title">Difference</div>
-          <div className="stat-value">- 0,74 zł / g</div>
-          <div className="stat-desc">↘︎ 0,99%</div>
+          <div className="stat">
+            <div className="stat-title">Difference</div>
+            <div className="stat-value">{priceDifference} zł / g</div>
+            <div className="stat-desc">
+              {percentageDifference > 0 ? "↗︎" : "↘︎"} {percentageDifference}%
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
